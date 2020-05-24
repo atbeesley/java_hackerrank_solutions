e880afe4b0c4804099ca1726a4ba70c5fd198197
atbeesley <alexander.beesley@gmail.com>
2020-05-24 12:27:41 +0100

    adds stdin_stdout_2

---
+++ stdin_stdout_2.java
@@ -0,0 +1,21 @@
+// Read an integer, a double, and a String from stdin, then print the values. There are three lines of output:
+//
+// On the first line, print String: followed by the unaltered String read from stdin.
+// On the second line, print Double: followed by the unaltered double read from stdin.
+// On the third line, print Int: followed by the unaltered integer read from stdin.
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
